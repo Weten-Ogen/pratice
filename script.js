294ff0df330b3278786aa76033cfeb27b142210f
Weten-ogen <alphaoware@gmail.com>
2021-10-29 15:12:16 +0000

    My own practice on javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+document.write("hello world")
+//window.print()
+console.log(window.screen.width)
+console.log(window.history)
+var find= document.getElementById("hello");
+console.log(find);
+console.log(document.lastModified)
+console.log(Date())
+console.log(window.innerHeight)
+console.log(window.pageXOffset)
+console.log(window.location)
+console.log(window.document)
+console.log(window.screen)
+//window.alert("Ok")
+//window.open("./scorpion/index.html")
+console.log(document.title)
+console.log(document.URL)
+console.log(document.domain)
+let myString = "communication";
+let miles= myString.indexOf("n");
+console.log(miles)
+var saying = "home sweet home";
+console.log(saying.split( " "))
+var origin =10.23456;
+console.log(origin.toFixed(3))
+console.log(origin.toPrecision(4))
+console.log(Math.PI)
+let can = Math.round(Math.PI);
+console.log(can)
+console.log(Math.sqrt(49))
